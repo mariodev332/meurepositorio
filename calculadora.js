33fb813c4b59cdf3cc43f570f9cc61c0b7673c8a
mario oliveira <mariobekman@gmail.com>
2020-08-31 01:34:17 -0300

    calculadora 2.0

---
+++ calculadora.js
@@ -0,0 +1,13 @@
+let valor
+let resultado
+function botao(num){
+    valor = document.calc.visor.value += num
+}
+function reseta(){
+    document.calc.visor.value = ''
+}
+function calcula(){
+    let resultado = eval(visor.value)
+   visor.value = resultado
+}
+    
